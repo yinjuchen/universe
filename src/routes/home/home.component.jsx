7fd2f20cce4bafddcef228156a0132e14a70c200
Tiffany Chen <tiffanychen@Tiffanys-MBP.localdomain>
2024-04-15 15:39:50 +0800

    updated minor

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -15,7 +15,7 @@ const Home = () => {
       <div className='links-container'>
         Dive into my <Link to="/projects" className='project-link'>Projects,</Link>
         <a className="resume-link" 
-           href='/Resume_Chen YinJu 2020.pdf'    
+           href='/Resume_ChenYinJu2024.pdf'    
            target="_blank"
            rel="noopener noreferrer">
            Resume,
