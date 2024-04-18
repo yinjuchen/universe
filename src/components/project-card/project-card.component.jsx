f47634f0a6794cd61621d0f6308235878d15efd6
Tiffany Chen <tiffanychen@Tiffanys-MBP.localdomain>
2024-04-18 22:48:08 +0800

    updated project links

--- src/components/project-card/project-card.component.jsx
+++ src/components/project-card/project-card.component.jsx
@@ -2,12 +2,15 @@ import './project-card.styles.scss'
 const ProjectCard = ({data}) => {
   return (
     <div className="project-card-container">
-       <img src={data.imageUrl} alt={data.name}/>
+      <a href={data.projectLink} className='project-image-link' target='_blank' rel='noreferrer'>
+        <img src={data.imageUrl} alt={data.name}/>
+      </a>
+       <a href={data.projectLink} className='project-title-link' target='_blank' rel='noreferrer'>
         <h2 className='project-name'>{data.name}</h2>
+      </a>
         <div className='project-link-container'>
-          <a href={data.projectLink} className='project-link'target='_blank' rel='noreferrer'>Discover</a>
+        <a href={data.sourceLink} className='source-link' target='_blank' rel='noreferrer'>View Source</a>
         </div>
-        <a href={data.sourceLink} className='source-link' target='_blank' rel='noreferrer'>View Source Code</a>
     </div>
   )
 }
